73191a38ed7c45899995b4a91d5fcb5317d43dcb
Joe Bowbeer <joe.bowbeer@gmail.com>
2025-08-04 17:36:11 -0700

    crossplane v2.0.0-rc.0.396

--- scripts/devtools_provision.sh
+++ scripts/devtools_provision.sh
@@ -27,6 +27,6 @@ sudo mv argocd /usr/local/bin/
 # argocd admin dashboard -n argocd
 
 helm upgrade --install crossplane -n crossplane-system --create-namespace \
-  https://charts.crossplane.io/master/crossplane-2.0.0-rc.0.373.g8c5886280.tgz
+  https://charts.crossplane.io/master/crossplane-2.0.0-rc.0.396.gcfd2fdc1f.tgz
 
 echo "[END] Install dev tools"
