ab318f8dfc887cf71e4aec908da9b2b860622faa
Joe Bowbeer <joe.bowbeer@gmail.com>
2025-12-14 22:00:26 +0000

    upgrade deps

--- scripts/devtools_provision.sh
+++ scripts/devtools_provision.sh
@@ -14,10 +14,13 @@ kind create cluster --name crossplane-playground
 # helm repo add headlamp https://kubernetes-sigs.github.io/headlamp/
 # helm upgrade --install headlamp headlamp/headlamp --namespace kube-system
 
-export ARGOCD_VERSION=v3.1.1
+export ARGOCD_VERSION=v3.2.1
+
+# TODO: arkade can install argocd core? 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/$ARGOCD_VERSION/manifests/core-install.yaml
 
+# TODO: arkade get argocd
 curl -sSL -o argocd https://github.com/argoproj/argo-cd/releases/download/$ARGOCD_VERSION/argocd-linux-amd64
 chmod +x argocd
 sudo mv argocd /usr/local/bin/
@@ -26,6 +29,7 @@ sudo mv argocd /usr/local/bin/
 # argocd login --core
 # argocd admin dashboard -n argocd
 
+# TODO: arkade install crossplane
 helm upgrade --install crossplane -n crossplane-system --create-namespace \
   https://charts.crossplane.io/stable/crossplane-2.0.2.tgz
 
